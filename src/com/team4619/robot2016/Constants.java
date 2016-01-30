216955512417ec73ea2744f743bc792a0f266fa3
TwelveBelowZero <Robotics@sw10.OVHS.HBUHSD.AD>
2016-01-30 11:01:49 -0800

    Moved Objects
    Moved objects from constants class to robotmap

--- src/com/team4619/robot2016/Constants.java
+++ src/com/team4619/robot2016/Constants.java
@@ -9,30 +9,6 @@ import edu.wpi.first.wpilibj.VictorSP;
 
 public class Constants {
 
-	//created xboxcontroller object 
-	public static Joystick xBoxController = new Joystick(0);
-
-	//creates four motors
-	public static VictorSP frontLeft = new VictorSP(0);
-	public static VictorSP frontRight = new VictorSP(1);
-	public static VictorSP backLeft = new VictorSP(2);
-	public static VictorSP backRight = new VictorSP(3);
-
-	//creates servo for bumping the ball forward
-	public static Servo bumper = new Servo(1);
 	
-	//creates two shooters
-	public static VictorSP leftShooter = new VictorSP(4);
-	public static VictorSP rightShooter = new VictorSP(5);
-	
-	//creates a climber
-	public static Victor climber = new Victor(7);
-	
-	//creates actuator using TalonSRX
-	public static TalonSRX actuator = new TalonSRX(6);
-
-	
-	//creates robot drive
-	public static RobotDrive driveMain = new RobotDrive(frontLeft, frontRight, backLeft, backRight);
 
 }
--- src/com/team4619/robot2016/OI.java
+++ src/com/team4619/robot2016/OI.java
@@ -1,37 +1,51 @@
 package com.team4619.robot2016;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-    //// CREATING BUTTONS
-    // One type of button is a joystick button which is any button on a joystick.
-    // You create one by telling it which joystick it's on and which button
-    // number it is.
-    // Joystick stick = new Joystick(port);
-    // Button button = new JoystickButton(stick, buttonNumber);
-    
-    // There are a few additional built in buttons you can use. Additionally,
-    // by subclassing Button you can create custom triggers and bind those to
-    // commands the same as any other Button.
-    
-    //// TRIGGERING COMMANDS WITH BUTTONS
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three ways:
-    
-    // Start the command when the button is pressed and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenPressed(new ExampleCommand());
-    
-    // Run the command while the button is being held down and interrupt it once
-    // the button is released.
-    // button.whileHeld(new ExampleCommand());
-    
-    // Start the command when the button is released  and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenReleased(new ExampleCommand());
+	//// CREATING BUTTONS
+	// One type of button is a joystick button which is any button on a joystick.
+	// You create one by telling it which joystick it's on and which button
+	// number it is.
+	// Joystick stick = new Joystick(port);
+	// Button button = new JoystickButton(stick, buttonNumber);
+
+	// There are a few additional built in buttons you can use. Additionally,
+	// by subclassing Button you can create custom triggers and bind those to
+	// commands the same as any other Button.
+
+	//// TRIGGERING COMMANDS WITH BUTTONS
+	// Once you have a button, it's trivial to bind it to a button in one of
+	// three ways:
+
+	// Start the command when the button is pressed and let it run the command
+	// until it is finished as determined by it's isFinished method.
+	// button.whenPressed(new ExampleCommand());
+
+	// Run the command while the button is being held down and interrupt it once
+	// the button is released.
+	// button.whileHeld(new ExampleCommand());
+
+	// Start the command when the button is released  and let it run the command
+	// until it is finished as determined by it's isFinished method.
+	// button.whenReleased(new ExampleCommand());
+
+	public Joystick xBoxController = new Joystick(1);
+	public Button A = new JoystickButton(xBoxController, 1),
+			B = new JoystickButton(xBoxController, 2),
+			X = new JoystickButton(xBoxController, 3),
+			Y = new JoystickButton(xBoxController, 4),
+			lBumper = new JoystickButton(xBoxController, 5),
+			rBumper = new JoystickButton(xBoxController, 6),
+			Back = new JoystickButton(xBoxController, 7),
+			Start = new JoystickButton(xBoxController, 8),
+			leftJoystickButton = new JoystickButton(xBoxController, 8),
+			rightJoystickButton = new JoystickButton(xBoxController, 8);
 }
 

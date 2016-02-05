b441fc2aa3c136c126e05c7657188b09b759bf3b
YazziJaramillo <Robotics@sw9.OVHS.HBUHSD.AD>
2016-02-04 20:57:26 -0800

    Created Climber Subsystem
    A climber subsystem was to ensure that a Climber subsystem existed.

--- src/com/team4619/robot2016/subsystems/ClimberSubsystem.java
+++ src/com/team4619/robot2016/subsystems/ClimberSubsystem.java
@@ -1,32 +1,38 @@
 package com.team4619.robot2016.subsystems;
 
 import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
 import com.team4619.robot2016.*;
 
-public class ClimberSubsystem {
+public class ClimberSubsystem extends Subsystem {
 	
+	protected static ClimberSubsystem instance;
+	public static ClimberSubsystem getInstance() {
+		if (ClimberSubsystem.instance == null) {
+			ClimberSubsystem.instance = new ClimberSubsystem();
+		}
+		
+		return ClimberSubsystem.instance;
+	}
+
+	@Override
+	public void initDefaultCommand() {
+		// TODO Auto-generated method stub
+	}
+
 	public void Extend()
 	{
-		if((RobotMap.xBoxController).getRawAxis(8)>0)
 		{
 			RobotMap.climber.set(Constants.Range(Constants.CLIMBER_MOTOR_SPEED_DUTY_CYCLE + .22, -1, 1));
-		}
-		else
-		{
-			RobotMap.climber.set(0);
+
 		}
 		
 	}
 	
 	public void Retract()
 	{
-		if((RobotMap.xBoxController).getRawAxis(7)>0)
-		{
+	
 			RobotMap.climber.set(-Constants.CLIMBER_MOTOR_SPEED_DUTY_CYCLE);
-		}
-		else
-	{
-			RobotMap.climber.set(0);
-		}
+	
 	}
-}

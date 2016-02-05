398bc084fa00f558af85d82c004d1adfe05d8515
TwelveBelowZero <Robotics@sw11.OVHS.HBUHSD.AD>
2016-02-04 18:53:23 -0800

    Fixed Issuess
    recreated PWM port constants

--- src/com/team4619/robot2016/Constants.java
+++ src/com/team4619/robot2016/Constants.java
@@ -41,6 +41,18 @@ public class Constants {
 		
 	}
 	
+	public class PWMPorts {
+		public static final int PWM_PORT_ZERO = 0;
+		public static final int PWM_PORT_ONE = 1;
+		public static final int PWM_PORT_TWO = 2;
+		public static final int PWM_PORT_THREE = 3;
+		public static final int PWM_PORT_FOUR = 4;
+		public static final int PWM_PORT_FIVE = 5;
+		public static final int PWM_PORT_SIX = 6;
+		public static final int PWM_PORT_SEVEN = 7;
+		public static final int PWM_PORT_EIGHT = 8;
+		public static final int PWM_PORT_NINE = 9;
+	}
 	
 	public static double Range(double value, double min, double max) {
 		return Math.min(max, Math.max(value, min));
